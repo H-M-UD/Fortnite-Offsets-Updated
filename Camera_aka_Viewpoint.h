b42dba609d4740cabe0e10dc919d4e1354f84934
H&M <140843935+H-M-UD@users.noreply.github.com>
2023-09-12 18:55:18 +0100

    Update Camera_aka_Viewpoint.h

--- Camera_aka_Viewpoint.h
+++ Camera_aka_Viewpoint.h
@@ -1,15 +1,28 @@
-CamewaDescwipsion GetViewPoint(ProtectedClass* p)
+struct Camera
 {
-    __int64 result; // rax
-    __int64 v7; // rcx
- 
-    result = p->Read<__int64>(base_address + 0xE2CED18);
-    v7 = base_address + 0xE2CECD8;
-    if (result)
-        v7 = result;
- 
-    __int64 CameraBuffer = p->Read<__int64>(p->Read<__int64>(v7) + 8) ^ 0x4CF1F15DFE2D977Fi64;
-    return p->Read<CamewaDescwipsion>(CameraBuffer);
+	float FieldOfView;
+	Vector3 Rotation;
+	Vector3 Location;
+};
+struct CamewaDescwipsion
+{
+	Vector3 Location;
+	Vector3 Rotation;
+	float FieldOfView;
+	char Useless[0x18];
+}; Camera vCamera;
+CamewaDescwipsion GetViewPoint()
+{
+	__int64 result; // rax
+	__int64 v7; // rcx
+
+	result = your_read<__int64>(your_baseaddress + 0xE2CED18);
+	v7 = your_baseaddress + 0xE2CECD8;
+	if (result)
+		v7 = result;
+
+	__int64 CameraBuffer = driver.read<__int64>(your_read<__int64>(v7) + 8) ^ 0x4CF1F15DFE2D977Fi64;
+	return your_read<CamewaDescwipsion>(CameraBuffer);
 }
 
 //join my server to support me <3

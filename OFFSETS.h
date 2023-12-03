71cb7589bd4ed45f5c778e0bd2b8964018d13bb5
H&M <140843935+H-M-UD@users.noreply.github.com>
2023-12-03 12:37:26 +0100

    Update OFFSETS.h

--- OFFSETS.h
+++ OFFSETS.h
@@ -2,11 +2,33 @@ UWORLD : 0x1123AF78
 FNAME : 0x11400C80
 BONEMATRIX : 0x149B864
 FIND_OBJECT : 0x3EDBF1C
+PersistentLevel = 0x30,
+OwningGameInstance = 0x1d0,
+PlayerCameraManager = 0x348,
+LocalPlayers = 0x38,
+AcknowledgedPawn = 0x338,
+PlayerState = 0x2b0,
+PlayerController = 0x30,
+PlayerArray = 0x2a8,
+RootComponent = 0x198,
+RelativeLocation = 0x128,
+AActor = 0x98,
 CurrentWeapon = 0xa20
-WeaponData = 0x4a8
+WeaponData = 0x4a8,
+Mesh = 0x318,
+Levels = 0x170,
+Tier = 0x73,
+TeamIndex = 0x10e0
+GameState = 0x158
 ReviveFromDBNOTime = 0x4ae0
-IsDBNO = 0x93a
+PawnPrivate = 0x308
+ServerWorldTimeSecondsDelta = 0x2c8
+bIsDBNO = 0x93a
+IsBot = 0x29a
 AmmoCount = 0xdcc
+BoneArray = 0x610
+BoneCache = 0x648
+ComponentToWorld = 0x240
 
 https://discord.gg/hm-pasta
 

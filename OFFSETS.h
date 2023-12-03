1dc6f9b42e08da5ff23116c95337f30e37830d35
H&M <140843935+H-M-UD@users.noreply.github.com>
2023-12-03 12:26:56 +0100

    Update OFFSETS.h

--- OFFSETS.h
+++ OFFSETS.h
@@ -1,53 +1,12 @@
-#define GWorld 0x10AEC0B8
-#define VIEW_POINT 0xE49C7E0
+UWORLD : 0x1123AF78
+FNAME : 0x11400C80
+BONEMATRIX : 0x149B864
+FIND_OBJECT : 0x3EDBF1C
+CurrentWeapon = 0xa20
+WeaponData = 0x4a8
+ReviveFromDBNOTime = 0x4ae0
+IsDBNO = 0x93a
+AmmoCount = 0xdcc
 
+https://discord.gg/hm-pasta
 
-namespace OFFSETS
-{
-    uintptr_t UWORLD = 0x10AEC0B8;
-    uintptr_t Viewpoint = 0xE49C7E0;
-
-    uintptr_t Gameinstance = 0x1b8;
-    uintptr_t LocalPlayers = 0x38;
-    uintptr_t PlayerController = 0x30;
-    uintptr_t LocalPawn = 0x338;
-    uintptr_t PlayerState = 0x2b0;
-    uintptr_t RootComponet = 0x198;
-    uintptr_t GameState = 0x158;
-    uintptr_t PersistentLevel = 0x30;
-    uintptr_t LastSubmitTime = 0x368;
-    uintptr_t LastRenderTimeOnScreen = 0x370;
-
-    uintptr_t ActorCount = 0xA0;
-    uintptr_t Cameramanager = 0x348;
-    uintptr_t AActor = 0x98;
-    uintptr_t CurrentActor = 0x8;
-    uintptr_t Mesh = 0x318;
-    uintptr_t Revivefromdbnotime = 0x4b68;
-    uintptr_t TeamId = 0x10e0;
-    uintptr_t ActorTeamId = 0x10e0;
-
-    uintptr_t IsDBNO = 0x872;
-    uintptr_t LocalActorPos = 0x128;
-    uintptr_t ComponetToWorld = 0x240;
-    uintptr_t BoneArray = 0x610;
-    uintptr_t Bonecache = 0x648;
-    uintptr_t Velocity = 0xb8;
-    uintptr_t Private = 0x308; //PawnPrivate
-    uintptr_t PlayerArray = 0x2A8;
-    uintptr_t relativelocation = 0x128;
-    uintptr_t UCharacterMovementComponent = 0x318;
-    uintptr_t entity_actor = 0x310;
-    uintptr_t bIsReloadingWeapon = 0x358;
-    uintptr_t GlobalAnimRateScale = 0xA80;
-    uintptr_t CurrentWeapon = 0x948;
-    uintptr_t Wireframe = 0x194;
-    uintptr_t SkeletalMeshes = 0x56e;
-    uintptr_t PawnMaterials_ALL = 0x5A60;
-
-    // for novoline paste mfs:
-
-    uintptr_t BoneMatrix = 0x11F2D70;
-    uintptr_t StaticFindObject = 0x1216928;
-}
-https://discord.gg/p824eVknXX

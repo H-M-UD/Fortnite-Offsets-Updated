7b26c65160a0e7000e0317c036c512b3d5f276ca
H&M <140843935+H-M-UD@users.noreply.github.com>
2023-12-21 08:10:30 +0100

    Update OFFSETS.h

--- OFFSETS.h
+++ OFFSETS.h
@@ -1,39 +1,75 @@
-UWORLD : 0x11781328 //UPDATED
-FNAME : 0x11400C80
-FreeFn: 0xC566C28
-GetBoneMatrix: 0x149B864
-ProjectWorldToScreen: 0x35BD6A8
-GObjects: 0x11213D40
-GetNameByIndex: 0x1004854
-LineOfSightTo: 0x65403D0
-FIND_OBJECT : 0x3EDBF1C
-PersistentLevel = 0x30,
-OwningGameInstance = 0x1d0,
-PlayerCameraManager = 0x348,
-LocalPlayers = 0x38,
-AcknowledgedPawn = 0x338,
-PlayerState = 0x2b0,
-PlayerController = 0x30,
-PlayerArray = 0x2a8,
-RootComponent = 0x198,
-RelativeLocation = 0x128,
-AActor = 0x98,
-CurrentWeapon = 0xa20
-WeaponData = 0x4a8,
-Mesh = 0x318,
-Levels = 0x170,
-Tier = 0x73,
-TeamIndex = 0x10e0
-GameState = 0x158
-ReviveFromDBNOTime = 0x4ae0
-PawnPrivate = 0x308
-ServerWorldTimeSecondsDelta = 0x2c8
-bIsDBNO = 0x93a
-IsBot = 0x29a
-AmmoCount = 0xdcc
-BoneArray = 0x610
-BoneCache = 0x648
-ComponentToWorld = 0x240
+"UWorld": "0x11781328",
+    "FName": "0x11947880",
+    "tier": "0x73",
+    "DisplayName": "0x90",
+    "PrimaryPickupItemEntry": "0x350",
+    "ItemDefinition": "0x18",
+    "ObjectID": "0x18",
+    "OwningGameInstance": "0x1d0",
+    "Levels": "0x170",
+    "GameState": "0x158",
+    "PersistentLevel": "0x30",
+    "AActors": "0xA0",
+    "ActorCount": "0xA8",
+    "LocalPlayers": "0x38",
+    "PlayerController": "0x30",
+    "AcknowledgedPawn": "0x338",
+    "PlayerCameraManager": "0x348",
+    "bHidden": "0x58",
+    "RootComponent": "0x198",
+    "RelativeLocation": "0x120",
+    "RelativeRotation": "0x138",
+    "Mesh": "0x318",
+    "PlayerState": "0x2b0",
+    "TeamIndex": "0x10e0",
+    "ReviveFromDBNOTime": "0x4ae8",
+    "LocalActorPos": "0x120",
+    "ComponentToWorld": "0x230",
+    "CurrentWeapon": "0xa20",
+    "CurrentVehicle": "0x2840",
+    "bIsReloadingWeapon": "0x360",
+    "GlobalAnimRateScale": "0xa68",
+    "WeaponData": "0x4a8",
+    "BoneArray": "0x608",
+    "BoneCache": "0x658",
+    "Velocity": "0x168",
+    "TriggerType": "0xb4c",
+    "ReticleDefaultColor": "0x6f8",
+    "ReticleEnemyColor": "0x6fc",
+    "ReticleBuildingColor": "0x700",
+    "ReticleNoTargetColor": "0x704",
+    "InputYawScale": "0x538",
+    "InputPitchScale": "0x53c",
+    "AmmoCount": "0xdcc",
+    "bIsParachuteOpen": "0x1fa4",
+    "bIsSkydiving": "0x1fa3",
+    "bIsDBNO": "0x93a",
+    "bIsDying": "0x758",
+    "bAlreadySearched": "0xd92",
+    "bActive": "0x2c0",
+    "bPickedUp": "0x5f1",
+    "KillScore": "0x10f4",
+    "TeamKillScore": "0x103c",
+    "WorldSettings": "0x2a0",
+    "WorldGravityZ": "0x310",
+    "AimPitchMin": "0x1620",
+    "AimPitchMax": "0x1624",
+    "JumpStaminaCost": "0x2594",
+    "PrimaryFireSound1P": "0x7b8",
+    "PrimaryFireSound": "0x7c0",
+    "PrimaryFireStopSound1P": "0x7d8",
+    "PrimaryFireStopSound": "0x7e0",
+    "VehicleAttributes": "0x1228",
+    "FrontLateralFrictionScale": "0x00",
+    "RearLateralFrictionScale": "0x04",
+    "BrakeForceTractionScale": "0x08",
+    "ForwardForceTractionScale": "0x0c",
+    "SlopeAntigravityScale": "0x10",
+    "TopSpeedScale": "0x14",
+    "VehicleGravityScale": "0x18",
+    "bBrakeBeforeReverse": "0x1c",
+    "CachedFuelComponent": "0x1250",
+    "ServerFuel": "0xf0"
 
 //https://discord.gg/hm-pasta
 

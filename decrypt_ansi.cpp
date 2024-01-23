dbfaefd4f81bcdc8c12649ef9c7c8d47c515b531
H&M <140843935+H-M-UD@users.noreply.github.com>
2024-01-23 11:56:55 +0000

    Update decrypt_ansi.cpp

--- decrypt_ansi.cpp
+++ decrypt_ansi.cpp
@@ -1,23 +1,17 @@
-            char* v2; // rdi
-            int v4; // er8
-            __int64 result; // rax
-            __int64 v6; // rdx
-            unsigned int v7; // er8
- 
-            v2 = m_buffer;
-            v4 = 14;
-            result = m_length;
-            if (result)
-            {
-                v6 = (unsigned int)result;
-                do
-                {
-                    v7 = v4 + 45297;
-                    *v2 = v7 + ~*v2;
-                    result = v7 << 8;
-                    v4 = result | (v7 >> 8);
-                    ++v2;
-                    --v6;
-                } while (v6);
-            }
-//https://discord.gg/hm-pasta
+char* v2 = buff; // rdi
+int v4 = 17; // ecx
+int v5 = nameLength; // eax
+__int64 v6; // rdx
+
+if (v5)
+{
+	v6 = v5;
+	do
+	{
+		v5 = v4 + ~*v2;
+		v4 += 0x1F0B;
+		*v2++ = v5;
+		--v6;
+	} while (v6);
+}
+//https://discord.gg/XyzVnsUZtv

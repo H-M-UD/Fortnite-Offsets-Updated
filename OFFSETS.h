df2bc1d91b216081c45c33b5dcab2be4e1b22a68
H&M <140843935+H-M-UD@users.noreply.github.com>
2024-01-31 15:31:45 +0100

    Update OFFSETS.h

--- OFFSETS.h
+++ OFFSETS.h
@@ -1,5 +1,5 @@
 //https://discord.gg/h-m
-"UWorld": "0x1133C4C8", //Up to date
+"UWorld": "0x1133C4C8  or  0x112F14C8", //Not Sure
     "FNamePool": "0x1150A280", //Up to date
     "tier": "0x73", //Up to date
     "DisplayName": "0x90", //Up to date

301c6797c9513526a29496313fe51d46b2866caa
H&M <140843935+H-M-UD@users.noreply.github.com>
2024-03-13 05:23:01 +0000

    Update OFFSETS.h

--- OFFSETS.h
+++ OFFSETS.h
@@ -1,151 +1,63 @@
+
+// https://discord.gg/h-m
+namespace SDK {
+ 
+    int UWorlds = 0x10C628D8;
+    int Fname = 0x12144040;
+    int Gameinstance = 0x1d8; // Class Engine.World -> struct UGameInstance
+ 
+    int LocalPlayers = 0x38; // Class Engine.GameInstance -> struct TArray<struct ULocalPlayer*> LocalPlayers; 
+    int PlayerController = 0x30; // Class Engine.Player - > struct APlayerController
+ 
+    int LocalPawn = 0x338; // Class Engine.PlayerController - > struct APawn* AcknowledgedPawn 
+ 
+    int PlayerState = 0x2b0; // Class Engine.Pawn - > struct APlayerState* PlayerState;
+    int RootComponet = 0x198;  //Class Engine.Actor -> struct USceneComponent* RootComponent;
+    int GameState = 0x160; // Class Engine.World -> struct AGameStateBase* GameState;
+    int PersistentLevel = 0x30; // Class Engine.World -> struct ULevel* PersistentLevel;
+    
+    int Velocity = 0x168; // Class Engine.SceneComponent -> struct FVector ComponentVelocity;  
+    int GunName = 0x98;
+    int IsDBNO = 0x93a; //    FortniteGameClass.AFortPawn -> char bIsDBNO : 1 
+    int IsDying = 0x758; //    FortniteGameClass.AFortPawn -> char bIsDying : 1 
+ 
+    int Cameramanager = 0x348; // Class Engine.PlayerController -> struct APlayerCameraManager* PlayerCameraManager; 
+    int Mesh = 0x318; // Class Engine.Character struct USkeletalMeshComponent* Mesh x318(0x08) 
+    int Revivefromdbnotime = 0x4c78; // FortniteGameClass.AFortPlayerPawnAthena - > float ReviveFromDBNOTime; 
+    int TeamId = 0x1251;  // FortniteGameClass.AFortPlayerStateAthena - > char TeamIndex;  
+    int pawn_private = 0x308; // Class Engine.PlayerState -> struct APawn* PawnPrivate;  
+    int bIsABot = 0x29a; // Class Engine.PlayerState -> char bIsABot : 1;  
+    int RelativeLocation = 0x120; // Class Engine.SceneComponent -> struct FVector RelativeLocation;  
+    int PlayerArray = 0x2a8; // Class Engine.GameStateBase -> struct TArray<struct APlayerState*> PlayerArray;  
+    int DefaultFOV = 0x2a4; //Engine_classes.APlayerCameraManager - > DefaultFOV 
+    int AActor = 0xA0;
+    int ActorCount = 0xA8;
+    int ComponetToWorld = 0x1c0;
+    int BoneArray = 0x598;
+    int LAST_SUMBIT_TIME = 0x2E8;
+    int LAST_SUMBIT_TIME_ON_SCREEN = 0x2F0;
+    int Levels = 0x170;
+ 
+    namespace FortWeapon {
+ 
+        int CurrentWeapons = 0xa20;// Class FortniteGame.FortPawn struct AFortWeapon* CurrentWeapon
+        int WeaponData = 0x4f0; // Class FortniteGame.FortWeapon struct UFortWeaponItemDefinition* WeaponData
+        int ItemName = 0x38; // Class ItemizationCoreRuntime.ItemDefinitionBase -> //struct FText ItemName;
+        int Tier = 0x13b; // Class FortniteGame.FortItemDefinition - > enum class EFortItemTier Tier
+        int bIsReloadingWeapon = 0x388; // Class FortniteGame.AFortWeapon -> bool bIsReloadingWeapon;
+        int AmmoCount = 0xe54; // Class FortniteGame.AActor -> int32_t AmmoCount;
+        int bIsTargeting = 0x581; // Class FortniteGame.CustomCharacterPartAnimInstance -> char bIsTargeting : 1;
+ 
+ 
+    }
+    namespace Character {
+        int bIsCrouched = 0x45c; // Class Engine.Character -> char bIsCrouched : 1;
+    }
+    namespace FortPawn {
+ 
+        int bIsJumping = 0xa18; //Class FortniteGame.AFortPawn - > char bIsJumping : 1; 
+        int bIsSliding = 0x75b; //Class FortniteGame.AFortPawn - > char bIsSliding : 1;
+        int TargetedFortPawn = 0x1890;  //Class FortniteGame.AFortPlayerController - > TargetedFortPawn
+    }
+}
 //https://discord.gg/h-m
-[+] : UWorld -> 0x11791DF8
-  [+] : GObjects -> 0x1176AB00
-  [+] : GNames -> 0x119692C0
-  [+] : AActors -> 0xA0
-  [+] : ACharacter::Mesh -> 0x318
-  [+] : AActor::RootComponent -> 0x198
-  [+] : AActor::CustomTimeDilation -> 0x68
-  [+] : UPlayer::PlayerController -> 0x30
-  [+] : UGameInstance::LocalPlayers -> 0x38
-  [+] : UWorld::OwningGameInstance -> 0x1D0
-  [+] : UWorld::GameState -> 0x158
-  [+] : UWorld::PersistentLevel -> 0x30
-  [+] : UWorld::Levels -> 0x170
-  [+] : UEngine::TinyFont -> 0x30
-  [+] : UEngine::SmallFont -> 0x50
-  [+] : UEngine::LargeFont -> 0x90
-  [+] : APlayerState::PawnPrivate -> 0x308
-  [+] : APlayerState::CompressedPing -> 0x298
-  [+] : APlayerState::PlayerId -> 0x294
-  [+] : APlayerState::bIsABot ( BitField Index -> 3 ) -> 0x29A
-  [+] : APawn::PlayerState -> 0x2B0
-  [+] : AFortPlayerPawnAthena::ReviveFromDBNOTime -> 0x4B80
-  [+] : AFortPlayerPawnAthena::bCanBeMarkedAsTeammate -> 0x5AB0
-  [+] : AFortPlayerPawn::CurrentVehicle -> 0x28B8
-  [+] : AFortPlayerPawn::bIsSkydiving -> 0x1FB3
-  [+] : AFortPlayerPawn::bIsParachuteOpen -> 0x1FB4
-  [+] : ACameraActor::FOVAngle -> 0x2B0
-  [+] : ACameraActor::AspectRatio -> 0x2AC
-  [+] : ACameraActor::CameraComponent -> 0x298
-  [+] : AFortAthenaVehicle::VehicleAttributes -> 0x1208
-  [+] : AFortAthenaVehicle::CachedFuelComponent -> 0x1230
-  [+] : AFortAthenaVehicle::TopSpeedCurrentMultiplier -> 0xA68
-  [+] : AFortAthenaVehicle::PushForceCurrentMultiplier -> 0xA6C
-  [+] : AFortAthenaVehicle::WaterEffectsVehicleMaxSpeedKmh -> 0x8FC
-  [+] : AFortAthenaVehicle::CachedSpeed -> 0xE08
-  [+] : UFortVehicleFuelComponent::ServerFuel -> 0xF0
-  [+] : AFortAthenaSKPushVehicle::ForwardForceTractionScale -> 0x1F6C
-  [+] : FReplicatedAthenaVehicleAttributes::FrontLateralFrictionScale -> 0x0
-  [+] : FReplicatedAthenaVehicleAttributes::RearLateralFrictionScale -> 0x4
-  [+] : FReplicatedAthenaVehicleAttributes::BrakeForceTractionScale -> 0x8
-  [+] : FReplicatedAthenaVehicleAttributes::ForwardForceTractionScale -> 0xC
-  [+] : FReplicatedAthenaVehicleAttributes::SlopeAntigravityScale -> 0x10
-  [+] : FReplicatedAthenaVehicleAttributes::TopSpeedScale -> 0x14
-  [+] : FReplicatedAthenaVehicleAttributes::VehicleGravityScale -> 0x18
-  [+] : FReplicatedAthenaVehicleAttributes::bBrakeBeforeReverse -> 0x1C
-  [+] : AGameStateBase::PlayerArray -> 0x2A8
-  [+] : AFortPickup::PrimaryPickupItemEntry -> 0x350
-  [+] : FFortItemEntry::ItemDefinition -> 0x18
-  [+] : UFortItemDefinition::Tier -> 0x12B
-  [+] : UItemDefinitionBase::ItemName -> 0x38
-  [+] : APlayerController::AcknowledgedPawn -> 0x338
-  [+] : APlayerController::PlayerCameraManager -> 0x348
-  [+] : APlayerController::InputPitchScale -> 0x53C
-  [+] : APlayerController::InputYawScale -> 0x538
-  [+] : APlayerController::InputRollScale -> 0x540
-  [+] : APlayerCameraManager::DefaultFOV -> 0x2A4
-  [+] : APlayerCameraManager::ViewPitchMin -> 0x2384
-  [+] : APlayerCameraManager::ViewPitchMax -> 0x2388
-  [+] : APlayerCameraManager::ViewYawMin -> 0x238C
-  [+] : APlayerCameraManager::ViewYawMax -> 0x2390
-  [+] : APlayerCameraManager::ViewRollMin -> 0x2394
-  [+] : APlayerCameraManager::ViewRollMax -> 0x2398
-  [+] : APlayerCameraManager::ViewTarget -> 0x320
-  [+] : ABuildingWeakSpot::bActive ( BitField Index -> 2 ) -> 0x2C0
-  [+] : ABuildingWeakSpot::HitCount -> 0x2C4
-  [+] : ABuildingContainer::bAlreadySearched ( BitField Index -> 2 ) -> 0xD92
-  [+] : ULocalPlayer::ViewportClient -> 0x78
-  [+] : AFortPawn::CurrentWeapon -> 0xA20
-  [+] : AFortPawn::PreviousWeapon -> 0xA28
-  [+] : AFortPawn::CurrentWeaponList -> 0xA30
-  [+] : AFortPawn::bIsDying ( BitField Index -> 4 ) -> 0x758
-  [+] : AFortPawn::bIsDBNO ( BitField Index -> 4 ) -> 0x93A
-  [+] : AFortWeapon::WeaponData -> 0x4D0
-  [+] : AFortWeapon::CurrentReticleColor -> 0xD28
-  [+] : AFortWeapon::AmmoCount -> 0xE1C
-  [+] : AFortWeapon::ChargeTime -> 0xE30
-  [+] : AFortWeapon::AccumulatedChargeTime -> 0xE34
-  [+] : AFortWeapon::LastChargeTime -> 0xE38
-  [+] : AFortWeapon::bIsReloadingWeapon -> 0x368
-  [+] : AFortWeapon::bIsChargingWeapon -> 0x369
-  [+] : AFortWeapon::bIsEquippingWeapon -> 0x350
-  [+] : AFortWeaponRanged::AimPitchMin -> 0x1770
-  [+] : AFortWeaponRanged::AimPitchMax -> 0x1774
-  [+] : FFortRangedWeaponStats::Spread -> 0x150
-  [+] : FFortRangedWeaponStats::OverrideBaseSpread -> 0x154
-  [+] : FFortRangedWeaponStats::SpreadDownsights -> 0x158
-  [+] : FFortRangedWeaponStats::OverrideSpreadDownsights -> 0x15C
-  [+] : FFortRangedWeaponStats::StandingStillSpreadMultiplier -> 0x160
-  [+] : FFortRangedWeaponStats::AthenaCrouchingSpreadMultiplier -> 0x164
-  [+] : FFortRangedWeaponStats::AthenaSlidingSpreadMultiplier -> 0x168
-  [+] : FFortRangedWeaponStats::AthenaJumpingFallingSpreadMultiplier -> 0x16C
-  [+] : FFortRangedWeaponStats::AthenaSprintingSpreadMultiplier -> 0x170
-  [+] : FFortRangedWeaponStats::MinSpeedForSpreadMultiplier -> 0x174
-  [+] : FFortRangedWeaponStats::MaxSpeedForSpreadMultiplier -> 0x178
-  [+] : FFortRangedWeaponStats::SpreadDownsightsAdditionalCooldownTime -> 0x17C
-  [+] : FFortRangedWeaponStats::SpreadExponent -> 0x180
-  [+] : FFortRangedWeaponStats::RecoilVert -> 0x1F8
-  [+] : FFortRangedWeaponStats::RecoilHoriz -> 0x208
-  [+] : FFortRangedWeaponStats::FiringRate -> 0x1D0
-  [+] : FFortBaseWeaponStats::ReloadTime -> 0xE8
-  [+] : FFortTrapStats::ArmTime -> 0x150
-  [+] : FFortTrapStats::FireDelay -> 0x154
-  [+] : FFortTrapStats::DamageDelay -> 0x158
-  [+] : AFortPlayerControllerGameplay::bIsCreativeModeratorModeEnabled ( BitField Index -> 5 ) -> 0x3142
-  [+] : AFortPlayerController::bAutoRunOn ( BitField Index -> 3 ) -> 0xF55
-  [+] : AFortPlayerController::OnSetFirstPersonCamera -> 0xF80
-  [+] : AFortWeapon::bAllowTargeting ( BitField Index -> 3 ) -> 0xD80
-  [+] : AFortWeapon::TimeToEquip -> 0x340
-  [+] : UFortWeaponItemDefinition::TriggerType -> 0xAFC
-  [+] : UFortWeaponItemDefinition::HitNotifyDuration -> 0xB04
-  [+] : UFortWeaponItemDefinition::bAllowTargetingDuringReload ( BitField Index -> 6 ) -> 0xAFF
-  [+] : AFortWeapon::bIgnoreTryToFireSlotCooldownRestriction -> 0x13A9
-  [+] : UMaterial::Wireframe ( BitField Index -> 6 ) -> 0x1B8
-  [+] : UMaterial::BlendMode -> 0x129
-  [+] : UMaterialInstance::ScalarParameterValues -> 0x160
-  [+] : UMaterialInstance::VectorParameterValues -> 0x170
-  [+] : AWorldSettings::WorldGravityZ -> 0x310
-  [+] : UFortClientSettingsRecord::HUDScale -> 0x4D8
-  [+] : UFortClientSettingsRecord::InitialHUDScale -> 0x4E0
-  [+] : UFortClientSettingsRecord::ScopedSlow -> 0x564
-  [+] : UFortClientSettingsRecord::TargetingSlow -> 0x560
-  [+] : UFortClientSettingsRecord::MouseSensitivity -> 0x58C
-  [+] : UFortClientSettingsRecord::MouseSensitivityX -> 0x590
-  [+] : UFortClientSettingsRecord::MouseSensitivityY -> 0x594
-  [+] : UFortClientSettingsRecord::MusicVolume -> 0x918
-  [+] : UFortClientSettingsRecord::MasterVolume -> 0x914
-  [+] : UFortClientSettingsRecord::FOVMinimum -> 0x9B4
-  [+] : UFortClientSettingsRecord::FOVMaximum -> 0x9B8
-  [+] : UFortClientSettingsRecord::bAnonymousMode -> 0x93D
-  [+] : USceneComponent::RelativeRotation -> 0x138
-  [+] : USceneComponent::RelativeLocation -> 0x120
-  [+] : USceneComponent::ComponentVelocity -> 0x168
-  [+] : USceneComponent::RelativeScale3D -> 0x150
-  [+] : USceneComponent::PhysicsVolume -> 0xA8
-  [+] : USceneComponent::bAbsoluteLocation ( BitField Index -> 2 ) -> 0x180
-  [+] : USkinnedMeshComponent::bForceWireframe ( BitField Index -> 5 ) -> 0x75E
-  [+] : USkinnedMeshComponent::SkeletalMesh -> 0x540
-  [+] : USkeletalMeshComponent::GlobalAnimRateScale -> 0x9F8
-  [+] : AFortPlayerStateAthena::TeamIndex -> 0x10F1
-  [+] : AFortPlayerStateAthena::KillScore -> 0x1104
-  [+] : AFortPlayerStateAthena::TeamKillScore -> 0x104C
-  [+] : AFortPlayerStateAthena::bThankedBusDriver ( BitField Index -> 0 ) -> 0x125D
-  [+] : AFortPlayerStateAthena::SquadId -> 0x125C
-  [+] : AFortPlayerStateAthena::bIsTalking -> 0x1318
-  [+] : AFortPlayerStateAthena::bIsMuted -> 0x1319
-  [+] : AFortPlayerStateAthena::bIsDisconnected -> 0x15F4
-  [+] : AFortPlayerStateAthena::RebootCounter -> 0x178C
-  [+] : AFortPlayerStateAthena::AlwaysIncludeTeamAsAllies -> 0x10F0
-  [+] : AFortPlayerState::Platform -> 0x438
-   //https://discord.gg/h-m

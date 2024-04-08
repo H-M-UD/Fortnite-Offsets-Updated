b059ca0f7100e39f05609a1d492626fc1beb9171
H&M <140843935+H-M-UD@users.noreply.github.com>
2024-04-08 15:49:55 +0000

    Update Camera_aka_Viewpoint.h

--- Camera_aka_Viewpoint.h
+++ Camera_aka_Viewpoint.h
@@ -1,29 +1,43 @@
-Decrypt Ansi
-
-char*v2; // rdi
-  int v4; // ebx
-  unsigned int v5; // er9
-  unsigned int v6; // er8
-  __int64 result; // rax
-  unsigned int v8; // er9
-  int v9; // ecx
-
-  v2 = buffer;
-  v4 = length;
-  v5 = sub_14473C460(); //0x130D7A4
-  v6 = v5 ^ 0x9C677CC5;
-  result = 1041204193 * v5;
-  v8 = v5 % 0x21;
-  v9 = 0;
-  if ( v4 )
-  {
-    do
-    {
-      result = v9 + v8;
-      ++v9;
-      v6 += result;
-      *v2++ ^= v6;
-    }
-    while ( v9 < v4 );
-  }
 //https://discord.gg/h-m
+//https://discord.gg/hm-services
+struct cdecrypt
+{
+	Vector3 location;
+	Vector3 rotation;
+	float fov;
+};
+
+cdecrypt viewpoint()
+{
+	
+	uintptr_t cachedgworld = request->read<uintptr_t>(globals->imagebase + offsets->gworld);
+	uintptr_t cachedgameinstance = request->read<uintptr_t>(cachedgworld + offsets->gameinstance);
+	uintptr_t cachedlocalplayers = request->read<uintptr_t>(cachedgameinstance + offsets->localplayers);
+	uintptr_t cachedlocalplayer = request->read<uintptr_t>(cachedlocalplayers);
+	uintptr_t cachedplayercontroller = request->read<uintptr_t>(cachedlocalplayer + offsets->playercontroller);
+
+	cdecrypt camera;
+
+	auto locationp = request->read<uintptr_t>(cachedgworld + 0x110);
+	auto rotationp = request->read<uintptr_t>(cachedgworld + 0x120);
+
+	struct fnrotation
+	{
+		double a;
+		double b;
+		double c;
+	}fnrotation;
+
+	fnrotation.a = request->read<double>(rotationp);
+	fnrotation.b = request->read<double>(rotationp + 0x20);
+	fnrotation.c = request->read<double>(rotationp + 0x1d0);
+
+	camera.location = request->read<Vector3>(locationp);
+	camera.rotation.x = asin(fnrotation.c) * (180.0 / M_PI);
+	camera.rotation.y = ((atan2(fnrotation.a * -1, fnrotation.b) * (180.0 / M_PI)) * -1) * -1;
+	camera.fov = request->read<float>((uintptr_t)cachedplayercontroller + 0x394) * 90.f;
+
+	return camera;
+}
+//https://discord.gg/h-m
+//https://discord.gg/hm-services

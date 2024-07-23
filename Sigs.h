e8378dc6788b647d0ec74ff30016a26143669e85
H&M <140843935+H-M-UD@users.noreply.github.com>
2024-07-23 17:05:44 +0100

    Update Sigs.h

--- Sigs.h
+++ Sigs.h
@@ -7,19 +7,17 @@ GObjects: 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1
 [NEW] GetNameByIndex: 48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 33 ED 48 8B FA 48 89 2A 48 89 6A 08 8B
 LineOfSightTo: 48 8B C4 48 89 58 20 55 56 57 41 54 41 56 48 8D 68
 StaticFindObject: 48 89 74 24 ?? 48 89 7C 24 ?? 55 41 54 41 55 41 56 41 57 48 8B EC 48 83 EC ?? 4C 8B E9 48 8D 4D E8
-//https://discord.gg/h-m
+//https://discord.gg/hmpasta
 VTables:
-[NEW] DrawTransition: 0x70 [112]
-[NEW] ViewportClient: 0x71 [113]
-[NEW] PostRender: 0x6F [111]
-[NEW] ProcessEvent: 0x4C [76]
-GetPlayerViewPoint: 0x7F0
-SetControlRotation: 0x760
-GetCameraLocation: 0x808
-GetCameraRotation: 0x800
-GetFOVAngle: 0x7C0
-LineOfSightTo: 0x770
-[NEW] AddPitchInput: 0xCF0
-[NEW] ClientSetCameraMode: 0xA08
-[NEW] AddYawInput: 0xCF8
-//https://discord.gg/h-m
+int DrawTitleSafeArea = 0x388 / 0x8;
+int ProcessEvent = 0x268 / 0x8;
+int GetMaterial = 0x2D0 / 0x8;
+int GetPlayerViewPoint = 0x7F8 / 0x8;
+int GetCameraViewPoint = 0x2C8 / 0x8;
+int GetViewPoint = 0x2C8 / 0x8;
+int IsAllowedToRender = 0x118 / 0x8;
+int FireSingle = 0x888 / 0x8;
+int TryToFire = 0x548 / 0x8;
+int GetDamageStartLocation = 0x7C8 / 0x8;
+int GetFiringRate = 0x9E0 / 0x8;
+//https://discord.gg/hmpasta
